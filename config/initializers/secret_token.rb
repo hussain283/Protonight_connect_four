6e9baef6e8c5034c85c7256546439f681e613794
Hussain Tambawala <hussain283@gmail.com>
2013-08-12 20:12:41 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConnectFour::Application.config.secret_token = '0c835cc94e14c2a04d3a0f0578b55425631bc8db4b8d2b3029638c067e3b1295f8c9149bf894bec5166faae5697f82bd254acea74219c69e0b76e97a3246e0b1'
